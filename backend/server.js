a0b0d68cfc7f6c535303bc3b1a66a737e023249a
olinanderson <olin.anderson@ucalgary.ca>
2022-01-17 22:02:49 -0700

    Fixing static file issues in server.js

--- backend/server.js
+++ backend/server.js
@@ -73,10 +73,10 @@ app.use("/api/buySell", require("./routes/api/buySell"));
 if (process.env.NODE_ENV === "production") {
   productionMode = true;
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build"));
   });
 } else {
   productionMode = false;
