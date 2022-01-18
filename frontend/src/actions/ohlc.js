b7de6c9c673357558004ac9d67cb628cb36bb42b
olinanderson <olin.anderson@ucalgary.ca>
2022-01-17 19:32:22 -0700

    Added/fixed axios api requests to backend

--- frontend/src/actions/ohlc.js
+++ frontend/src/actions/ohlc.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 import { GET_OHLC, GET_OHLC_FAIL } from "./types";
 
-export const getOHLC = () => async (dispatch) => {
+export const getOHLC = (day) => async (dispatch) => {
   try {
-    let res = await axios.get("/api/realtime/ohlc/initial");
+    let res = await axios.get("/api/ohlc", {
+      params: {
+        day: day
+      }
+    });
 
     await dispatch({
       type: GET_OHLC,
